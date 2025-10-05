8a8bce19c91b4968da75c3055fea750257b0ef52
jumagu <jmgg.gutierrez@gmail.com>
2025-10-04 22:10:28 -0500

    feat: implement movie search functionality in SearchMovieDelegate and integrate with CustomAppBar

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,8 +1,16 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/format_number.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
+typedef SearchMoviesCallback =
+    Future<List<Movie>> Function({required String query});
+
 class SearchMovieDelegate extends SearchDelegate<Movie?> {
+  final SearchMoviesCallback searchMoviesFn;
+
+  SearchMovieDelegate({required this.searchMoviesFn});
+
   @override
   String? get searchFieldLabel => 'Search movie...';
 
@@ -33,6 +41,88 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return const Text('BuildSuggestions');
+    return FutureBuilder(
+      future: searchMoviesFn(query: query),
+      builder: (context, snapshot) {
+        final suggestedMovies = snapshot.data ?? [];
+
+        return ListView.builder(
+          itemCount: suggestedMovies.length,
+          itemBuilder: (context, index) {
+            final suggestedMovie = suggestedMovies[index];
+
+            return _MovieSearchItem(movie: suggestedMovie);
+          },
+        );
+      },
+    );
+  }
+}
+
+class _MovieSearchItem extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieSearchItem({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: size.width * 0.05, vertical: 5),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: size.width * 0.025,
+        children: [
+          // ? Movie Image
+          SizedBox(
+            width: size.width * 0.25,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Image.network(movie.posterPath),
+            ),
+          ),
+
+          // ? Movie Description
+          SizedBox(
+            width: size.width * 0.6,
+            child: Column(
+              spacing: 1.5,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  movie.title,
+                  maxLines: 2,
+                  style: textStyles.titleMedium?.copyWith(
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Text(
+                  movie.overview,
+                  maxLines: 2,
+                  style: TextStyle(overflow: TextOverflow.ellipsis),
+                ),
+                Row(
+                  spacing: 2,
+                  children: [
+                    Icon(
+                      Icons.star_half_rounded,
+                      color: Colors.yellow.shade800,
+                    ),
+                    Text(
+                      FormatNumber.short(movie.voteAverage, 1),
+                      style: textStyles.bodyMedium?.copyWith(
+                        color: Colors.yellow.shade900,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/presentation/widgets/shared/custom_app_bar.dart
+++ lib/presentation/widgets/shared/custom_app_bar.dart
@@ -1,13 +1,17 @@
 import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CustomAppBar extends StatelessWidget {
+class CustomAppBar extends ConsumerWidget {
   const CustomAppBar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
+
+    final searchMovies = ref.read(moviesRepositoryProvider).searchMovies;
 
     return SafeArea(
       bottom: false,
@@ -23,7 +27,10 @@ class CustomAppBar extends StatelessWidget {
               Spacer(), // Takes the available space
               IconButton(
                 onPressed: () {
-                  showSearch(context: context, delegate: SearchMovieDelegate());
+                  showSearch(
+                    context: context,
+                    delegate: SearchMovieDelegate(searchMoviesFn: searchMovies),
+                  );
                 },
                 icon: const Icon(Icons.search),
               ),
