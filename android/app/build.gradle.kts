df5b3e7257d5462750b165ba33a9ade30e174cdd
jumagu <jmgg.gutierrez@gmail.com>
2026-06-11 00:36:44 -0500

    chore: change bundle id

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.cinemapedia"
+    namespace = "com.jumagu.cinemapedia"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.cinemapedia"
+        applicationId = "com.jumagu.cinemapedia"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/jumagu/cinemapedia/MainActivity.kt
+++ android/app/src/main/kotlin/com/jumagu/cinemapedia/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.cinemapedia
+package com.jumagu.cinemapedia
 
 import io.flutter.embedding.android.FlutterActivity
 
